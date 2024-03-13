097252eddf1e5692cbae8ee6fb01899de2e8bd76
tongren <tongren@startdt.com>
2024-03-13 16:03:58 +0800

    build: update build config

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  base: './', // 将根路径换成相对路径
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
